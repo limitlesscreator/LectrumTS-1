52335ce4699b770c9078b25b71d7012b9aa00bf9
Vladislav V <0428vlad0428@mail.ru>
2021-08-30 12:23:45 +0300

    TS practice

--- HomeWork/interfaces.ts
+++ HomeWork/interfaces.ts
@@ -72,4 +72,63 @@ enum links {
 
 links.vk // 'https://vk.com/'
 
+// const createPassword = (name: string, age: number | string ): string => `${name}${age}`
+const createPassword = (name: string = 'Vladislav', age: number | string = 20): string => `${name}${age}`
+createPassword('Jack', 31)
+createPassword('Jack', '31 ')
+createPassword()
+
+const createPassword2 = (name: string, age?: number): string => `${name}${age}`
+createPassword2('Vlad')
+
+const createSkills = (name: string, ...skills: Array<string>): string => (
+    `${name}, my skils are ${skills.join()}`
+)
+
+createSkills('Jack', 'JS', 'ES6', 'React')
+
+const emptyObject = (): object => ({})
+
+const greetUser = (): void => {
+    console.log('You are looking good:)')
+}
+
+const msg = 'hello'
+const error = (msg: string): never => {
+    throw new Error(msg)
+}
+
+let myFunc: (firstArg: string) => void
+
+function oldFunc(name: string): void {
+    alert(`${name} how are u?`)
+}
+
+myFunc = oldFunc
+
+type personType = {
+    name: string
+    age: number
+    getPass?: () => string
+    nickName?: string
+}
+
+let user: personType = {
+    name: 'Vlad',
+    nickName: 'vlad0428',
+    age: 30,
+}
+
+let user2: { name: string, age: number } = {
+    name: 'Yauhen2',
+    age: 44,
+}
+
+let admin: personType = {
+    name: 'Max',
+    age: 31,
+    getPass(): string {
+        return `${this.name}${this.age}`
+    }
+}
 
